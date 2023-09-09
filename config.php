3a00cdbdef36125e8e10afacd2f3b4a2c1f62fff
Alexandreiavriani <61045318+Alexandreiavriani@users.noreply.github.com>
2023-09-09 23:13:34 +0400

    FreeWork

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$conn=mysqli_connect('localhost','root','','freelance') or die('connection failed');
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$conn -> set_charset("utf8");
-
-
-?>
